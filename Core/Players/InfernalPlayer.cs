a4ce210930ce4ee1c9e2e12ac78d0ac61813cf8a
Darian Hughes <darianmhughes7@gmail.com>
2026-02-24 20:57:41 -0600

    removed some old systems and merged trap damage revert into infernal player

--- Core/Players/InfernalPlayer.cs
+++ Core/Players/InfernalPlayer.cs
@@ -770,6 +770,37 @@ namespace InfernalEclipseAPI.Core.Players
             TryCoinDebuff();
         }
 
+        public override void ModifyHitByProjectile(Projectile proj, ref Terraria.Player.HurtModifiers modifiers)
+        {
+            //Reverts vanilla trap damage nerf
+            ref StatModifier sourceDamage = ref modifiers.SourceDamage;
+
+            if (proj.type == ProjectileID.Explosives)
+            {
+                sourceDamage /= Main.expertMode ? 0.225f : 0.35f;
+            }
+            else if (proj.type == ProjectileID.RollingCactus || proj.type == ProjectileID.RollingCactusSpike)
+            {
+                sourceDamage /= Main.expertMode ? 0.3f : 0.5f;
+            }
+
+            if (!Main.expertMode)
+                return;
+
+            if (proj.type == ProjectileID.Boulder || proj.type == ProjectileID.MiniBoulder)
+            {
+                sourceDamage /= 0.65f;
+            }
+            else if (proj.type == ProjectileID.SpikyBallTrap || proj.type == ProjectileID.FlamethrowerTrap || proj.type == ProjectileID.PoisonDartTrap)
+            {
+                sourceDamage /= 0.625f;
+            }
+            else if (proj.type == ProjectileID.SpearTrap)
+            {
+                sourceDamage /= 0.6f;
+            }
+        }
+
         public override void OnHitByProjectile(Projectile proj, Player.HurtInfo hurtInfo)
         {
             TryCoinDebuff();
