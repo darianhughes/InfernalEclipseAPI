35e1d6d2f8ee1446676036e49347752c2a12daee
Darian Hughes <darianmhughes7@gmail.com>
2026-06-09 09:41:36 -0500

    v0.10.6.1: - fixed some stuff with the ui when you don't have hypnos mod on

--- Common/InfernumScreens/HypnosDraedonScreen.cs
+++ Common/InfernumScreens/HypnosDraedonScreen.cs
@@ -2,6 +2,8 @@
 
 namespace InfernalEclipseAPI.Common.InfernumScreens
 {
+    [JITWhenModsEnabled("HypnosMod")]
+    [ExtendsFromMod("HypnosMod")]
     public class HypnosDraedonScreen : DraedonIntroScreen
     {
         public override bool ShouldBeActive() => NPC.AnyNPCs(ModLoader.GetMod("HypnosMod").Find<ModNPC>("Draedon").Type) && InfernumMode.InfernumMode.CanUseCustomAIs;
