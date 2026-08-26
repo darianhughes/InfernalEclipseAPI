1cd11bdf631fccd96d18e86c0a8ed117a75ebce8
Darian Hughes <darianmhughes7@gmail.com>
2026-08-26 17:25:24 -0500

    slight changes to match api format and to make it where mods wont fail to load without thorium

--- Core/Systems/Hooks/ILTileChanges/SOTSHydroponicsHooks.cs
+++ Core/Systems/Hooks/ILTileChanges/SOTSHydroponicsHooks.cs
@@ -1,7 +1,6 @@
 ﻿using SOTS;
 using Terraria.ID;
 using Terraria.ModLoader;
-using ThoriumMod;
 
 namespace InfernalEclipseAPI.Core.Systems.Hooks.ILTileChanges
 {
@@ -11,41 +10,7 @@ namespace InfernalEclipseAPI.Core.Systems.Hooks.ILTileChanges
     {
         public override void PostSetupContent()
         {
-            // Example registrations:
-            /* 
-            RegisterHydroponicsHerb(
-                ModContent.ItemType<YourModdedHerbItem>(), 
-                ModContent.TileType<YourModdedHerbTile>(), 
-                tileOffset (should be 0),
-                DustID.GrassBlades,
-                red light,
-                green light,
-                blue light
-            );
-            */
-            RegisterHydroponicsHerb(
-                ModContent.ItemType<ThoriumMod.Items.Depths.MarineKelp>(), 
-                ModContent.TileType<ThoriumMod.Tiles.MarineKelp>(), 
-                0, 
-                DustID.GrassBlades,
-                0f, 
-                0f, 
-                0f    
-            );
-        }
-
-        private static void RegisterHydroponicsHerb(int itemType, int tileType, int tileOffset = 0, int harvestDust = 0, float r = 0f, float g = 0f, float b = 0f)
-        {
-            InfernalCrossmod.SOTS.Mod?.Call(
-                "AddHydroponicsHerb",
-                itemType,
-                tileType,
-                tileOffset,
-                harvestDust,
-                r,
-                g,
-                b
-            );
+            //Example registration: InfernalCrossmod.SOTS.Mod?.Call("AddHydroponicsHerb", ModContent.ItemType<ThoriumMod.Items.Depths.MarineKelp>(), ModContent.TileType<MarineKelp>(), 0, DustID.GrassBlades, 0f, 0f, 0f);
         }
     }
 }
